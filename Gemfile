ce8d1dbda800e155387bfb4c954284234422beaf
James Dabbs <jamesdabbs@gmail.com>
2016-03-02 14:53:56 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+ruby   "2.2.4"
+
+gem "lita"
+gem "lita-slack"
+
+group :development do
+  gem "pry"
+end
---
+++ handlers/scoreboard.rb
@@ -0,0 +1,33 @@
+module Lita
+  module Handlers
+    class Scoreboard < Handler
+      route /(\S+)\+\+/, :plus, help: { "TEXT++" => "Add points" }
+      route /(\S+)\-\-/, :minus, help: { "TEXT--" => "Remove points" }
+      route /score (\S+)/, :score, help: { "score TEXT" => "Show points" }
+
+      def plus response
+        key    = response.matches[0][0]
+        score  = redis.get(key) || 0
+        score += 1
+        redis.set key, score
+        response.reply "#{key} now has #{score} points"
+      end
+
+      def minus response
+        key    = response.matches[0][0]
+        score  = redis.get(key) || 0
+        score -= 1
+        redis.set key, score
+        response.reply "#{key} now has #{score} points"
+      end
+
+      def score response
+        key   = response.matches[0][0]
+        score = redis.get(key) || 0
+        response.reply "#{key} has #{score} points"
+      end
+    end
+
+    Lita.register_handler Scoreboard
+  end
+end
