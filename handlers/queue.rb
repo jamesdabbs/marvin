8ce14381045a2106d53c7b74b39d6ed40f921cf6
Russell Osborne <russell@theironyard.com>
2016-03-03 18:01:07 -0500

    Switch to allow marvin to address users via mentions

--- handlers/queue.rb
+++ handlers/queue.rb
@@ -21,38 +21,38 @@ module Lita
         help: { "next up" => "removes oldest user from queue and ping group" }
       )
 
-      def add_to_queue(response)
-        target = response.matches.first
-        queue = queue_for(target)
-        queue << response.user.mention_name
-        persist_queue_for(target, queue)
-        send_queue_status(response, queue)
+      def add_to_queue(msg)
+        target = Lita::User.find_by_mention_name msg.matches[0][0]
+        queue = queue_for(target.id)
+        queue << msg.user.id
+        persist_queue_for(target.id, queue)
+        send_queue_status(target, msg, queue)
       end
 
-      def remove_from_queue(response)
-        target = response.matches.first
-        queue = queue_for(target)
-        queue.delete(response.user.mention_name)
+      def remove_from_queue(msg)
+        target = Lita::User.find_by_mention_name msg.matches[0][0]
+        queue = queue_for(target.id)
+        queue.delete(msg.user.id)
         persist_queue_for(target, queue)
-        send_queue_status(response, queue)
+        send_queue_status(target, msg, queue)
       end
 
-      def next(response)
-        target = response.user.mention_name
+      def next(msg)
+        target = Lita::User.find_by_id msg.user.id
         queue = queue_for(target)
         queue.shift
         persist_queue_for(target, queue)
-        send_queue_status(response, queue)
+        send_queue_status(target, msg, queue)
       end
 
-      def send_queue_status(response, queue)
+      def send_queue_status(responder, msg, queue)
         case queue.size
         when 0
-          response.reply("Nobody in the queue!")
+          msg.reply("#{slack_notifer responder.id} the queue is empty!")
         when 1
-          response.reply("Next up is @#{queue[0]}.")
+          msg.reply("#{slack_notifer queue[0]} is up for #{slack_notifer responder.id}.")
         else
-          response.reply("Next up is @#{queue[0]}, and then #{queue[1..-1].join(", ")}")
+          msg.reply("#{slack_notifer queue[0]} is up for #{slack_notifer responder.id}, and then #{queue[1..-1].map{|u| mention_name u}.join(", ")}")
         end
       end
 
@@ -64,6 +64,16 @@ module Lita
       def persist_queue_for(target, queue)
         redis.set target, queue.to_json
       end
+
+    private
+        def slack_notifer(id, type: :user)
+          char = type == :user ? '@' : "#"
+          "<#{char}#{id}>"
+        end
+
+        def mention_name(id)
+          Lita::User.find_by_id(id).mention_name
+        end
     end
     Lita.register_handler(Queue)
   end
--- spec/queue_spec.rb
+++ spec/queue_spec.rb
@@ -12,38 +12,38 @@ describe Lita::Handlers::Queue, lita_handler: true do
   it { is_expected.to route("next up") }
 
   it "lets everyone know who's in the queue when one user is waiting" do
-    bob = Lita::User.create(1, name: "bob")
-    lilly = Lita::User.create(2, name: "lilly")
+    bob = Lita::User.create(1, mention_name: "bob")
+    lilly = Lita::User.create(2, mention_name: "lilly")
     send_message("question for @lilly", as: bob)
-    expect(replies.last).to eq("Next up is @bob.")
+    expect(replies.last).to eq("<@1> is up for <@2>.")
   end
 
   it "lets everyone know who's in the queue" do
-    bob = Lita::User.create(1, name: "bob")
-    lilly = Lita::User.create(2, name: "lilly")
-    joe = Lita::User.create(3, name: "joe")
+    bob = Lita::User.create(1, mention_name: "bob")
+    lilly = Lita::User.create(2, mention_name: "lilly")
+    joe = Lita::User.create(3, mention_name: "joe")
     send_message("question for @lilly", as: bob)
     send_message("question for @lilly", as: joe)
-    expect(replies.last).to eq("Next up is @bob, and then joe")
+    expect(replies.last).to eq("<@1> is up for <@2>, and then joe")
   end
 
   it "removes someone from the queue when they say nm" do
-    bob = Lita::User.create(1, name: "bob")
-    lilly = Lita::User.create(2, name: "lilly")
-    joe = Lita::User.create(3, name: "joe")
+    bob = Lita::User.create(1, mention_name: "bob")
+    lilly = Lita::User.create(2, mention_name: "lilly")
+    joe = Lita::User.create(3, mention_name: "joe")
     send_message("question for @lilly", as: bob)
     send_message("question for @lilly", as: joe)
     send_message("nevermind @lilly", as: bob)
-    expect(replies.last).to eq("Next up is @joe.")
+    expect(replies.last).to eq("<@3> is up for <@2>.")
   end
 
   it "gets next up person" do
-    bob = Lita::User.create(1, name: "bob")
-    lilly = Lita::User.create(2, name: "lilly")
-    joe = Lita::User.create(3, name: "joe")
+    bob = Lita::User.create(1, mention_name: "bob")
+    lilly = Lita::User.create(2, mention_name: "lilly")
+    joe = Lita::User.create(3, mention_name: "joe")
     send_message("question for @lilly", as: bob)
     send_message("question @lilly", as: joe)
     send_message("next up", as: lilly)
-    expect(replies.last).to eq("Next up is @joe.")
+    expect(replies.last).to eq("<@2> the queue is empty!")
   end
 end
